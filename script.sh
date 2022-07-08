573fa2e72e593cf201e86bfb939d162ce6bfb50e
Mrweb <33089526+mrwebfr@users.noreply.github.com>
2022-07-08 03:22:42 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,58 @@
 #!/bin/bash
-source setup.sh
+
+function main() {
+    banner
+
+    PS3='
+Veuillez faire un choix : '
+
+    options=(
+        "Quitter"
+        "Mises à jour"
+        "Nettoyage"
+        "Thèmes"
+        "Applications"
+        "Outils"
+    )
+
+    select opt in "${options[@]}"
+    do
+        case $opt in
+            "Quitter")
+                # clear
+                exit
+                ;;
+            "Mises à jour")
+                run_script "scripts/dist_update.sh"
+                break
+                ;;
+            "Nettoyage")
+                run_script "scripts/dist_clean.sh"
+                break
+                ;;
+            "Thèmes")
+                run_script "scripts/themes.sh"
+                break
+                ;;
+            "Applications")
+                run_script "scripts/app.sh"
+                break
+                ;;
+            "Outils")
+                run_script "scripts/tools.sh"
+                break
+                ;;
+            *) echo "invalid option $REPLY";;
+        esac
+    done
+
+}
+
+cp /tmp/mw_tools/tools.sh ~/tools.sh
+chmod +x ~/tools.sh
+# clear
+cd /tmp/mw_tools/
+chmod +x scripts/functions.sh
+source scripts/functions.sh
+
+main
